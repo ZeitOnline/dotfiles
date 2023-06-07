52041c9002318068f621c956a34d3b46338ca5fb
Norman Stetter <norman>
2023-06-07 13:35:47 +0200

    maint: add new clusters

--- run_gcloud_credentials.sh
+++ run_gcloud_credentials.sh
@@ -13,6 +13,8 @@ gcloud container clusters get-credentials production --zone europe-west3-a --pro
 gcloud container clusters get-credentials staging --zone europe-west3-a --project zeitonline-main
 gcloud container clusters get-credentials devel --zone europe-west3-a --project zeitonline-main
 gcloud container clusters get-credentials zon-misc-prod-1 --zone europe-west3-a --project zeitonline-gke-misc-prod
+gcloud container clusters get-credentials ops-production --region europe-west3 --project zeitonline-main
+gcloud container clusters get-credentials main-general-23-01 --region europe-west3 --project zeitonline-main
 
 # Fetch GCP Application Default Credentials like Terraform uses 'em
 if ! gcloud auth application-default print-access-token &> /dev/null
