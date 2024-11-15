d7c0ae16db8e30f83e8d68d19da3189051ec0e7b
Andreas Zeidler <andreas.zeidler@zeit.de>
2024-11-15 13:46:27 +0100

    fix: only set ZON-specific env vars for interactive shells

--- private_dot_config/private_fish/conf.d/zon.fish
+++ private_dot_config/private_fish/conf.d/zon.fish
@@ -1,4 +1,10 @@
-set -x VAULT_ADDR https://vault.ops.zeit.de/
+# common ZON environment settings
+
+status is-interactive || exit
+
+# set vault URL
+set -xg VAULT_ADDR https://vault.ops.zeit.de/
+
 # prefer local kube config:
 set -xg KUBECONFIG ./kube.config:$HOME/.kube/config
 
