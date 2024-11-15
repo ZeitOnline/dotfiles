a95ea8739dc1bddcb266f58991f7606134fec4c1
Andreas Zeidler <andreas.zeidler@zeit.de>
2024-11-15 14:13:39 +0100

    fix: don't overwrite existing "git template dir" setting
    Refs: 3ec6cdf7

--- run_pre_commit_hooks.sh
+++ run_pre_commit_hooks.sh
@@ -3,6 +3,9 @@
 # this will install the regular ("pre-commit") and "commit-msg" hooks
 # also see https://pre-commit.com/#pre-commit-init-templatedir
 
-git config --global init.templateDir ~/.config/git/templates
+if [ -z "( git config --global init.templateDir )" ]; then
+    git config --global init.templateDir ~/.config/git/templates
+fi
+
 pre-commit init-templatedir ~/.config/git/templates/
 pre-commit init-templatedir --hook-type commit-msg ~/.config/git/templates/
