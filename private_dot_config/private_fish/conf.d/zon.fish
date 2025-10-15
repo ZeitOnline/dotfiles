4155f6f22290c8164b2ab56a74a5cf9e319bc7e2
Andreas Zeidler <andreas.zeidler@zeit.de>
2025-10-15 08:56:42 +0200

    chore: remove no longer needed (and outdated) cache configuration
    A provider cache is now used automatically.
    
    Refs: 73525c7f, aa66b747

--- private_dot_config/private_fish/conf.d/zon.fish
+++ private_dot_config/private_fish/conf.d/zon.fish
@@ -7,6 +7,3 @@ set -xg VAULT_ADDR https://vault.ops.zeit.de/
 
 # prefer local kube config:
 set -xg KUBECONFIG ./kube.config:$HOME/.kube/config
-
-# https://terragrunt.gruntwork.io/docs/features/provider-cache
-set -xg TERRAGRUNT_PROVIDER_CACHE true
