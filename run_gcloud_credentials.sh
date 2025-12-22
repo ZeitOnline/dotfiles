8d26e00cc4225b24e37bb6c9a8dc1e2e287ff23b
Juan V. García <111006213+jv-garcia@users.noreply.github.com>
2025-12-22 16:43:18 +0100

    chore: add ops devel cluster (#94)

--- run_gcloud_credentials.sh
+++ run_gcloud_credentials.sh
@@ -14,6 +14,7 @@ gcloud container clusters get-credentials main-production-25-01 --region europe-
 gcloud container clusters get-credentials production --zone europe-west3-a --project zeitonline-main
 gcloud container clusters get-credentials staging --zone europe-west3-a --project zeitonline-main
 gcloud container clusters get-credentials devel --zone europe-west3-a --project zeitonline-main
+gcloud container clusters get-credentials ops-devel-24-01 --zone europe-west3-a --project zeitonline-main
 gcloud container clusters get-credentials ops-production --region europe-west3 --project zeitonline-main
 gcloud container clusters get-credentials main-general-23-01 --region europe-west3 --project zeitonline-main
 
